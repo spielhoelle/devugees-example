42771558d1afefbfdbb13af8c294c14b0842094b
ayhamk94 <ayham.kteash1995@gmail.com>
2017-07-11 13:11:25 +0200

    steps are now pulled form external html file with Ajax

--- js/steps.js
+++ js/steps.js
@@ -1,19 +1,42 @@
 $(document).ready(function() {
-  console.log('Java Loaded');
-  var width = 0;
-  var button = $('.step button');
-  var progressBar = $('.progress-bar');
-  button.on('click', function() {
-    var nextStep = $(this).closest('.step').next('.step');
-    nextStep.fadeIn('slow');
-    nextStep.removeClass('hidden');
-    $(this).fadeOut('slow',function() {
-      $(this).remove();
+
+  function toNextStep(steps) {
+    console.log(steps);
+    $('.steps').html(steps);
+
+    var width = 0;
+    var button = $('.step button');
+    var progressBar = $('.progress-bar');
+    $('.steps').on('click', 'button', function() {
+      var nextStep = $(this).closest('.step').next('.step');
+      nextStep.fadeIn('slow');
+      nextStep.removeClass('hidden');
+      $(this).fadeOut('slow', function() {
+        $(this).remove();
+      });
+      if (width < 100) {
+        width += 25;
+        progressBar.css('width', width + '%');
+        progressBar.text(width + '%')
+      }
     });
-    if (width < 100) {
-      width += 25;
-      progressBar.css('width', width + '%');
-      progressBar.text(width + '%')
+  }
+  $.ajax("./steps.html", {
+    timeout: 3000,
+    success: function(steps) {
+      setTimeout(function() {
+        toNextStep(steps);
+      }, 2000);
+    },
+    beforeSend: function() {
+      $(".alert").load("alert.html");
+    },
+    complete: function() {
+      setTimeout(function() {
+        $('.alert').html('<strong>info</strong>' + ' steps are now loaded!!').addClass('alert-success');
+
+      }, 2000)
     }
   });
+
 });
